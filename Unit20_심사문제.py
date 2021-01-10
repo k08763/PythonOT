fd6a1357144085b885a2762ad20fad9b06bde957
Seokhyeon <k08763@cau.ac.kr>
2021-01-11 02:39:26 +0900

    Unit 20 심사문제

---
+++ Unit20_심사문제.py
@@ -0,0 +1,10 @@
+st, sp = map(int, input().split())
+for i in range(st, sp+1):
+    if i%5==0 and i%7==0:
+        print('FizzBuzz')
+    elif i%5==0:
+        print('Fizz')
+    elif i%7==0:
+        print('Buzz')
+    else:
+        print(i)
