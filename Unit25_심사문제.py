ceeb7c72ac2ace9bdb4a096175e1298ded086856
Seokhyeon <k08763@cau.ac.kr>
2021-01-13 12:41:23 +0900

    Unit 25 심사문제

---
+++ Unit25_심사문제.py
@@ -0,0 +1,6 @@
+keys=input().split()
+values=map(int, input().split())
+x=dict(zip(keys, values))
+x={keys: values for keys, values in x.items() if keys!='delta'}
+x={keys: values for keys, values in x.items() if values!=30}
+print(x)
